56c4ebce44396a3c0c627ca2d6ea1dc5d40b5908
SrPabvliss <pablomartinvillacres@gmail.com>
2024-05-30 23:12:59 -0500

    Services infraestructure uptdate to work with the new db

--- src/services/infraestructure/http-server/models/update-service.req.ts
+++ src/services/infraestructure/http-server/models/update-service.req.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger'
 import { CreateServiceReq } from './create-service.req'
+import { IUpdateServiceDto } from 'src/services/domain/dtos/update-service.dto'
 
-export class UpdateServiceReq extends PartialType(CreateServiceReq) {}
+export class UpdateServiceReq
+  extends PartialType(CreateServiceReq)
+  implements IUpdateServiceDto {}
