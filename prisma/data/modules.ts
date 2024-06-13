746a274218cf218ce0984c197fa392e6bb7d55d1
Chu <dzhu2409@gmail.com>
2024-06-13 09:10:59 -0500

    Update modules seed

--- prisma/data/modules.ts
+++ prisma/data/modules.ts
@@ -6,53 +6,18 @@ interface Module {
 
 export const modules: Module[] = [
   {
-    name: 'Authentication',
-    description: 'Module for user authentication and authorization',
-    isActive: true,
-  },
-  {
-    name: 'User Management',
+    name: 'ADMNISTRATOR',
     description: 'Module for managing user accounts and profiles',
     isActive: true,
   },
   {
-    name: 'Product Catalog',
-    description: 'Module for managing products and their details',
+    name: 'SUPERVISOR',
+    description: 'Module for managing user roles and permissions',
     isActive: true,
   },
   {
-    name: 'Order Management',
-    description: 'Module for handling customer orders and transactions',
-    isActive: true,
-  },
-  {
-    name: 'Inventory Management',
-    description: 'Module for tracking and managing inventory levels',
-    isActive: true,
-  },
-  {
-    name: 'Reporting',
-    description: 'Module for generating reports and analytics',
-    isActive: true,
-  },
-  {
-    name: 'Customer Relationship Management (CRM)',
-    description: 'Module for managing customer interactions and relationships',
-    isActive: true,
-  },
-  {
-    name: 'Billing and Invoicing',
-    description: 'Module for managing billing and generating invoices',
-    isActive: true,
-  },
-  {
-    name: 'Shipping and Logistics',
-    description: 'Module for handling shipping and logistics operations',
-    isActive: true,
-  },
-  {
-    name: 'Content Management System (CMS)',
-    description: 'Module for managing website content and pages',
+    name: 'SALES',
+    description: 'Module for managing sales and customers',
     isActive: true,
   },
 ]
