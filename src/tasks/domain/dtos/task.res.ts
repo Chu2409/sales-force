7232324841dde100ad1fdb1bd63d9e8e1b7579aa
Emi1213 <emi1213galarza@gmail.com>
2024-06-25 18:39:43 -0500

    Update tasks

--- src/tasks/domain/dtos/task.res.ts
+++ src/tasks/domain/dtos/task.res.ts
@@ -1,3 +1,6 @@
+import { IDelegationRes } from 'src/delegations/domain/dtos/delegation.res'
 import { ITask } from '../models/task.interface'
 
-export interface ITaskRes extends ITask {}
+export interface ITaskRes extends Omit<ITask, 'delegation'> {
+  delegation: IDelegationRes
+}
--- src/tasks/infrastructure/http-server/models/create-task.req.ts
+++ src/tasks/infrastructure/http-server/models/create-task.req.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import {
   IsDate,
@@ -13,21 +14,26 @@ import { TaskStatus, TaskType } from 'src/tasks/domain/models/task.interface'
 export class CreateTaskReq implements ICreateTaskDto {
   @IsOptional()
   @IsEnum(TaskStatus)
+  @ApiProperty({ enum: TaskStatus, required: false })
   status?: TaskStatus
 
   @IsOptional()
   @IsInt()
   @IsPositive()
   @Min(15)
+  @ApiProperty({ minimum: 15, required: false })
   estimatedTime?: number
 
   @IsDate()
   @Type(() => Date)
+  @ApiProperty()
   date: Date
 
   @IsEnum(TaskType)
+  @ApiProperty({ enum: TaskType })
   type: TaskType
 
   @IsPositive()
+  @ApiProperty()
   delegationId: number
 }
