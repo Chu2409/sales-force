4d10c9692f0b1fcecc542784fc7c040defb5c904
Emi1213 <emi1213galarza@gmail.com>
2024-06-25 18:40:36 -0500

    Update items

--- src/items/infrastructure/http-server/models/create-item.req.ts
+++ src/items/infrastructure/http-server/models/create-item.req.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsOptional, IsInt, IsDefined } from 'class-validator'
 import { ICreateItemDto } from 'src/items/domain/dtos/create-item.dto'
 
 export class CreateItemReq implements ICreateItemDto {
   @IsOptional()
   @IsInt()
+  @ApiProperty({ required: false })
   discount?: number
 
   @IsOptional()
   @IsInt()
+  @ApiProperty({ required: false })
   productId?: number
 
   @IsOptional()
   @IsInt()
+  @ApiProperty({ required: false })
   serviceId?: number
 
   @IsInt()
   @IsDefined()
+  @ApiProperty()
   quantity: number
 }
