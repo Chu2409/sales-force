18314b343b5c19050d8ed1671f88982debf86fc7
Chu <dzhu2409@gmail.com>
2024-06-25 19:24:12 -0500

    Add documentation to auth module

--- src/auth/infrastructure/http-server/models/auth-login.req.ts
+++ src/auth/infrastructure/http-server/models/auth-login.req.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString, MinLength } from 'class-validator'
 import { IAuthLoginDto } from 'src/auth/domain/dtos/auth-login.dto'
 
 export class AuthLoginReq implements IAuthLoginDto {
   @IsString()
   @MinLength(4)
+  @ApiProperty({ minLength: 4 })
   username: string
 
   @IsString()
   @MinLength(6)
+  @ApiProperty({ minLength: 6 })
   password: string
 }
--- src/auth/infrastructure/http-server/models/auth.res.ts
+++ src/auth/infrastructure/http-server/models/auth.res.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { IAuthRes } from 'src/auth/domain/dtos/auth.res'
-import { IEmployeeRes } from 'src/employees/domain/dtos/employee.res'
+import { EmployeeRes } from 'src/employees/infrastructure/http-server/models/employee.res'
 
 export class AuthRes implements IAuthRes {
-  employee: IEmployeeRes
+  @ApiProperty()
+  employee: EmployeeRes
 
   @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' })
   token: string
